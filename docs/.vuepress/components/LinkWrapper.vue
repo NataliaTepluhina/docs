91f340ca056437f62cc2f9b8df781d29e7c62968
Natalia Tepluhina <tarya.se@gmail.com>
2019-02-23 17:36:59 +0200

    Added links

---
+++ docs/.vuepress/components/LinkWrapper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div :class="{'doc-link-wrapper': editorMode}">
+    <slot></slot>
+    <div class="doc-links" v-if="editorMode">
+      <a v-for="(value, key) in links" :href="value">{{ key }}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import links from "../links.js";
+export default {
+  props: {
+    entry: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      links: links[this.entry],
+      editorMode: links.editorMode
+    };
+  }
+};
+</script>
+
+<style>
+.doc-link-wrapper {
+  position: relative;
+  background-color: rgba(126, 255, 223, 0.363);
+}
+
+.doc-links {
+  position: absolute;
+  top: 0;
+  right: -35%;
+  padding: 10px;
+  border: 1px solid lightgray;
+}
+
+.doc-links a {
+  display: block;
+}
+</style>
+
+
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'VuePress links',
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/guide/' },
+      { text: 'API', link: '/guide/API.md' },
+    ],
+  },
+};
